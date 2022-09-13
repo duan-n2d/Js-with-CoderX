0a0f563daf6825753ab6e697c3ebf0886789c0b4
Nguyễn Duy Duẩn <95265668+duan-n2d@users.noreply.github.com>
2022-09-13 11:10:35 +0700

    Update BaiTap30.js

--- BaiTap30.js
+++ BaiTap30.js
@@ -4,3 +4,7 @@
  * 2. Sử dụng method extname để tìm extension (đuôi) của một đường dẫn (path) tới file nào đó.
  * Tra cứu: https://nodejs.org/dist/latest-v8.x/docs/api/path.html#path_path_extname_path
  */
+var path = require('path');
+var filePath = './data.json';
+var extname = path.extname(filePath);
+console.log(extname);
